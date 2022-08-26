4cfd8e0e6a71eae1fba465d8ea2777c1069ccbf1
RanjitsinhMule <muleranjitsinh7323@gmail.com>
2022-08-26 16:27:16 +0530

    added

---
+++ src/com/bridgelabz/Linecomparison.java
@@ -0,0 +1,25 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class Linecomparison {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison");
+		
+				Scanner scan =new Scanner(System.in);
+				System.out.println("Enter x1 value: ");
+				int x1=scan.nextInt();
+				System.out.println("Enter x2 value: ");
+				int x2=scan.nextInt();
+				System.out.println("Enter y1 value: ");
+				int y1=scan.nextInt();
+				System.out.println("Enter y2 value: ");
+				int y2=scan.nextInt();
+				int r= ((int)Math.pow((x2-x1), 2)+(int)Math.pow((y2-y1), 2));
+				double Length= Math.sqrt(r);
+				System.out.println("Length of Line1 is: " + r);
+
+	}
+
+}
