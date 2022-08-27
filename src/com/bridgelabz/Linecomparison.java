561370a4f7ee1511c929e5b639e8ff2918b4a02a
RanjitsinhMule <muleranjitsinh7323@gmail.com>
2022-08-27 15:22:49 +0530

    Equality check added

--- src/com/bridgelabz/Linecomparison.java
+++ src/com/bridgelabz/Linecomparison.java
@@ -18,8 +18,28 @@ public class Linecomparison {
 				int y2=scan.nextInt();
 				int r= ((int)Math.pow((x2-x1), 2)+(int)Math.pow((y2-y1), 2));
 				double Length= Math.sqrt(r);
-				System.out.println("Length of Line1 is: " + r);
+				System.out.println("Length of Line1 is: " + Length);
 
-	}
+				System.out.println("Enter a1 value: ");
+				int a1=scan.nextInt();
+				System.out.println("Enter a2 value: ");
+				int a2=scan.nextInt();
+				System.out.println("Enter b1 value: ");
+				int b1=scan.nextInt();
+				System.out.println("Enter b2 value: ");
+				int b2=scan.nextInt();
+				int res= ((int)Math.pow((a2-a1), 2)+(int)Math.pow((b2-b1), 2));
+				double Length1= Math.sqrt(res);
+				System.out.println("Length of Line2 is: " + Length1);
 
+				if(Length==Length1) {
+					System.out.println("Two lines are equal");
+				}
+				else {
+					System.out.println("Two lines are not equal");
+				}
+		}
+				
 }
+
+
